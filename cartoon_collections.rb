dc887377e00bf1a977e77853706994c8b54e87ac
Sonata Arrington <sarrington1009@gmail.com>
2020-01-26 23:02:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
   # Your code here
-  dwarves.each_with_index {|n, index| puts n.sort}
+  dwarves.each_with_index {|n, index| puts "#{index}n"}
 end
 
 def summon_captain_planet# code an argument here
