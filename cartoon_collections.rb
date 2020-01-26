7cb31343659ac9d195caee1e0b7de66ac9ec2108
Sonata Arrington <sarrington1009@gmail.com>
2020-01-26 23:31:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(calls_long)
   # Your code here
-  calls_long.each {|x| if x.count > 4 puts true}
+  calls_long.each {|x| if x.count > 4 p true}
 end
 
 def find_the_cheese# code an argument here
