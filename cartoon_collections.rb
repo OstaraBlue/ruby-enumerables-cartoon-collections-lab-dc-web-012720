7afbbd354111316a964e10df9ac434beeb08438c
Sonata Arrington <sarrington1009@gmail.com>
2020-01-26 23:48:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,8 +23,12 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
-    return true
+  cheese_types.map { |x| 
+    array.map { |n| 
+      if x == n 
+        return true
+      end
+    end 
   end
   nil
 end
